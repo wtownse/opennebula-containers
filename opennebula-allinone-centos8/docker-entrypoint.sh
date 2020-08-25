54c804d23a30b2c9b9f74c14b7b7cc61fd13fe63
wayne townsend <wayne.townsend@dell.com>
2020-08-25 12:31:19 -0400

    5.10.4 final

--- opennebula-allinone-centos8/docker-entrypoint.sh
+++ opennebula-allinone-centos8/docker-entrypoint.sh
@@ -1,37 +1,58 @@
 #!/bin/bash
 if [ ! -f /var/lib/one/.one/one_auth ]; then
+  mkdir -p /var/lib/one/.one
   echo "oneadmin:${ONEPASSWORD}" > /var/lib/one/.one/one_auth
 fi
+
+if [ ! -f /var/lib/one/.ssh/config ]; then
+mkdir -p /var/lib/one/.ssh
+cat >/var/lib/one/.ssh/config << EOL
+Host *
+StrictHostKeyChecking no
+UserKnownHostsFile /dev/null
+Port ${SSH_PORT:=2022}
+EOL
+fi
+
+if [ ! -f /var/lib/one/.ssh/id_rsa ]; then
+sudo -u oneadmin yes | sudo -u oneadmin ssh-keygen -q -t rsa -N '' -f /var/lib/one/.ssh/id_rsa >/dev/null
+fi
+
 if [ ! -f /var/lib/one/init.one ]; then
-  cp -rf /var_lib_one/one /var/lib/
-  cp -rf /var_lib_one/one/.one /var/lib/one/.one
-  cp -rf /usr_lib_one/* /usr/lib/one/
-  cp -rf /usr_share_one/* /usr/share/one/
-  cp -rf /conf/* /etc/one/
   source /ctrl/oned-conf.sh
   echo "initialized" > /var/lib/one/init.one
+  cp -au /var_lib_one/* /var/lib/one
+  cp -au /usr_lib_one/* /usr/lib/one
+  cp -au /usr_share_one/* /usr/share/one
+  cp -au /conf/* /etc/one
 fi
+
 if [ ! -f /var/lib/one/sunstoneinit.one ]; then
-  cp -rf /usr_share_one/* /usr/share/one/
-  cp -rf /var_lib_one/* /var/lib/one/
-  cp -rf /usr_lib_one/* /usr/lib/one/
-  cp -rf /conf/* /etc/one/
   source /ctrl/sunstone-conf.sh
   echo "initialized" > /var/lib/one/sunstoneinit.one
+  cp -au /var_lib_one/* /var/lib/one
+  cp -au /usr_lib_one/* /usr/lib/one
+  cp -au /usr_share_one/* /usr/share/one
+  cp -au /conf/* /etc/one
 fi
+
 if [ -f /var/lock/one/one ]; then
   rm -rf /var/lock/one/one
 fi
+
 if [ ! -f /etc/one/oned.conf ]; then
   cp -rf /conf/one/* /etc/one/
   /ctrl/oned-conf.sh
 fi
+
 if [ ! -f /etc/one/sunstone-server.conf ]; then
   /ctrl/sunstone-conf.sh
 fi
+
 if [[ ! -f /etc/one/onegate/onegate-server.conf ]]; then
   /ctrl/onegate-conf.sh
 fi
+
 if [[ ! -f /etc/one/onegate/onegate-server.conf ]]; then
   /ctrl/oneflow-conf.sh
 fi
